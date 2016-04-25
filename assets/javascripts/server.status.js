bff53a53d8448bbdd83c72d2bdb012fa88809953
Eurides Baptistella <eurides.baptistella@gmail.com>
2016-04-25 15:42:20 -0300

    Update server.status.js

--- assets/javascripts/server.status.js
+++ assets/javascripts/server.status.js
@@ -3,12 +3,27 @@
  */
 
 $(document).ready(
-    $.fn.serverStatus = function () {
+    $.fn.serverStatus = function() {
+        $.ajax({
+            type: 'GET',
+            dataType: 'json',
+            url: 'integrator0800/service/status',
+            //data : { json: JSON.stringify( task ) },
+            success: function(data) {
+                if (!$('#content').length) {
+                    $('body').append('<div id="content">1#content</div>');
+                }
 
-        $.get("integrator0800/server/status", function (data) {
-            $(".content").add(data);
-            alert("Load was performed.");
+                if (!$('#content > .alert').length) {
+                    $('body > #content').append('<div class="alert alert-danger" role="alert">2#alert<ul></ul></div>');
+                }
+    
+                $('body > #content > .alert > ul').html('<li>#'+new Date()+'</li>');
+            }
         });
+        
+        setTimeout($(document).serverStatus(), 5000);
+    };
 
-    }
+    $(document).serverStatus();
 );
