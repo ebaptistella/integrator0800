ac669abfe5596b165db92651059402d212833781
Eurides Baptistella <eurides.baptistella@gmail.com>
2016-04-25 12:18:00 -0300

    update about

--- init.rb
+++ init.rb
@@ -3,8 +3,8 @@ Redmine::Plugin.register :integrator0800 do
   author 'Eurides Baptistella'
   description 'Integração com 0800'
   version '0.0.1'
-  url 'http://sysmo.com.br'
-  author_url 'http://sysmo.com.br'
+  url ''
+  author_url 'https://about.me/euridesbaptistella'
 
   settings :default => {'empty' => true}, :partial => 'settings/integrator0800'
   menu :application_menu, :integrator0800, { :controller => 'consult', :action => 'index' }, :caption => 'Integrator 0800'
