b30ea21f62f7fe648575328c539f61070d6264df
Eurides Baptistella <eurides.baptistella@gmail.com>
2016-08-02 10:03:07 -0300

    Update service_controller.rb

--- app/controllers/service_controller.rb
+++ app/controllers/service_controller.rb
@@ -2,7 +2,7 @@ class ServiceController < SuperclassController
   unloadable
 
   before_filter :load_project
-  before_filter :authorize
+  #before_filter :authorize
 
   def status
     render :json => getServerStatus;
